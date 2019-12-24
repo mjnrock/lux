5ac8de3658b0d1023a7ce313f52689ee8c1a0561
Matt Nawrocki <mjnrock@umich.edu>
2019-12-24 10:59:36 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+### Packaging Commands
+`npm version patch|minor|major`
+`npm publish --access=public`
--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-require = require("esm")(module/*, options*/)
-module.exports = require("./main.js");
+try {
+    require = require("esm")(module/*, options*/)
+    module.exports = require("./main.js");
+} catch(e) {
+    module.exports = require("./index.js");
+}
