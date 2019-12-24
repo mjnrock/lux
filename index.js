9563196407163dc9682623e51bcd4cc9f33548d4
Matt Nawrocki <mjnrock@umich.edu>
2019-12-24 10:59:56 -0500

    init

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ try {
     require = require("esm")(module/*, options*/)
     module.exports = require("./main.js");
 } catch(e) {
-    module.exports = require("./index.js");
+    module.exports = require("./main.js");
 }
