766461f2d1938dfcc13274e9d074466140e7c6ef
Matt Nawrocki <mjnrock@umich.edu>
2019-12-24 10:42:33 -0500

    init

--- core/Condition.js
+++ core/Condition.js
@@ -2,24 +2,7 @@ import ClassDecorators from "./classDecorators";
 
 import Attribute from "./Attribute";
 
-export default class Condition extends ClassDecorators.StateEvents {
-    //? [ Function, Expected this.args.length, Supported types ]
-    static EnumType = {
-        EQUALS: [ "Equals", 1, [ "string", "number", "boolean" ] ],
-        NOT_EQUALS: [ "NotEquals", 1, [ "string", "number", "boolean" ] ],
-        BETWEEN: [ "Between", 2, [ "number" ] ],
-        IN: [ "In", null, [ "any" ] ],
-        NOT_IN: [ "NotIn", null, [ "any" ] ],
-        GT: [ "GT", 1, [ "number" ] ],
-        GTE: [ "GTE", 1, [ "number" ] ],
-        LT: [ "LT", 1, [ "number" ] ],
-        LTE: [ "LTE", 1, [ "number" ] ],
-        CONTAINS: [ "Contains", 1, [ "any" ] ],
-        NOT_CONTAINS: [ "NotContains", 1, [ "any" ] ],
-        MATCH: [ "Match", 1, [ "string", "number", "boolean" ] ],
-        UUID: [ "UUID", 1, [ "string" ] ],
-    };
-
+class Condition extends ClassDecorators.StateEvents {
     constructor(type, ...args) {
         super();
         
@@ -225,3 +208,23 @@ export default class Condition extends ClassDecorators.StateEvents {
         return false;
     }
 };
+
+
+//? [ Function, Expected this.args.length, Supported types ]
+Condition.EnumType = {
+    EQUALS: [ "Equals", 1, [ "string", "number", "boolean" ] ],
+    NOT_EQUALS: [ "NotEquals", 1, [ "string", "number", "boolean" ] ],
+    BETWEEN: [ "Between", 2, [ "number" ] ],
+    IN: [ "In", null, [ "any" ] ],
+    NOT_IN: [ "NotIn", null, [ "any" ] ],
+    GT: [ "GT", 1, [ "number" ] ],
+    GTE: [ "GTE", 1, [ "number" ] ],
+    LT: [ "LT", 1, [ "number" ] ],
+    LTE: [ "LTE", 1, [ "number" ] ],
+    CONTAINS: [ "Contains", 1, [ "any" ] ],
+    NOT_CONTAINS: [ "NotContains", 1, [ "any" ] ],
+    MATCH: [ "Match", 1, [ "string", "number", "boolean" ] ],
+    UUID: [ "UUID", 1, [ "string" ] ],
+};
+
+export default Condition;
