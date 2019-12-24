62f1166764939828054acb171bf5ca023d0ce96d
Matt Nawrocki <mjnrock@umich.edu>
2019-12-24 10:41:00 -0500

    npm init

---
+++ core/package.js
@@ -0,0 +1,28 @@
+import Decorators from "./decorators";
+import ClassDecorators from "./classDecorators";
+
+import Subscription from "./Subscription";
+
+import Attribute from "./Attribute";
+import TextAttribute from "./TextAttribute";
+import NumberAttribute from "./NumberAttribute";
+
+import Condition from "./Condition";
+import Proposition from "./Proposition";
+
+export default {
+    Decorators,
+    ClassDecorators,
+    Helper,
+
+    Subscription,
+
+    Attribute,
+    TextAttribute,
+    NumberAttribute,
+
+    Condition,
+    Proposition,
+
+    ImageSequencer
+};
