4a04ba7e44b5a1b53db95f52df4bbdd31cc4830b
Matt Nawrocki <mjnrock@umich.edu>
2020-01-03 11:42:32 -0500

    init

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import Core from "./core/package";
 
 const Lux = {
-    Core,
-
-    InjectLux() {
-        if(window !== void 0) {
-            window.Lux = Lux;
-        }
-        if(global !== void 0) {
-            global.Lux = Lux;
-        }
-    }
+    Core
 };
+
+//Inject Lux into globals
+if(window !== void 0) {
+    window.Lux = Lux;
+}
+if(global !== void 0) {
+    global.Lux = Lux;
+}
 
 export default Lux;
