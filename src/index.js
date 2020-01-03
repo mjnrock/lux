b9db2829264181f90202fef5d5311f8bc8a0cbe7
Matt Nawrocki <mjnrock@umich.edu>
2020-01-03 11:36:21 -0500

    init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import Core from "./core/package";
 
 export default {
-    Core
+    Core,
+
+    InjectLux() {
+        if(window !== void 0) {
+            window.Lux = Lux;
+        }
+        if(global !== void 0) {
+            global.Lux = Lux;
+        }
+    }
 };
