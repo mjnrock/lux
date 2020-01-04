4a47fe4fe32567731ffd3ee7a851ca62b097ef4a
Matt Nawrocki <mjnrock@umich.edu>
2020-01-03 22:25:02 -0500

    init

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const Lux = {
     Core
 };
 
+const Lumen = new Core.ClassDecorators.StateEvents();
+
 //Inject Lux into globals
 if(window !== void 0) {
     window.Lux = Lux;
+    window.Lumen = Lumen;
 }
 if(global !== void 0) {
     global.Lux = Lux;
+    global.Lumen = Lumen;
 }
 
 export default Lux;
