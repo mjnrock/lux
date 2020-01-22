86eb582f99df4280bbf43173438b9db9e8f35059
mjnrock <mjnrock@umich.edu>
2020-01-22 12:29:54 -0500

    init

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import Core from "./core/package";
 import Node from "./node/package";
 
-let n1 = new Node.Node();
-let n2 = new Node.Node();
+// let n1 = new Node.Node();
+// let n2 = new Node.Node();
 
-n2.setNext((event, result) => {
-    console.log("---------------");
-    console.log(event);
-    console.log(result);
-    console.log("---------------");
-});
-n1.subscribe(n2);
-// n1.listen("prop-change", (r, [ e ]) => console.log(e, r));
+// n2.setNext((event, result) => {
+//     console.log("---------------");
+//     console.log(event);
+//     console.log(result);
+//     console.log("---------------");
+// });
+// n1.subscribe(n2);
+// // n1.listen("prop-change", (r, [ e ]) => console.log(e, r));
 
-n1.prop("Cat", 5);
-n1.prop("Cat", 6);
+// n1.prop("Cat", 5);
+// n1.prop("Cat", 6);
 
 export default {
     Core,
