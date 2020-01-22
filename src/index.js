0cc4706d0afcadd410647cb083336891c66ca3b5
mjnrock <mjnrock@umich.edu>
2020-01-22 15:10:20 -0500

    init

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import Node from "./node/package";
 let n1 = new Node.Node();
 let n2 = new Node.Node();
 
-// n2.setNext(e => {
-//     console.log("---------------");
-//     console.log(e);
-//     console.log("---------------");
-// });
+n2.setNext(e => {
+    console.log("---------------");
+    console.log(e);
+    console.log("---------------");
+});
 n1.subscribe(n2);
 n1.listen("prop-change", (e) => console.log("LISTEN:", e.getPayload()));
 n1.watch("Cat", (e) => console.log("WATCH:", e.getPayload()));
