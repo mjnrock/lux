d07520de52425f259d1484a90c78a611351d28aa
mjnrock <mjnrock@umich.edu>
2020-01-27 15:53:44 -0500

    init

--- src/tests/MasterNode.test.js
+++ src/tests/MasterNode.test.js
@@ -8,21 +8,21 @@ export function RunTest() {
     
     let mn1 = new Node.MasterNode();
 
-    // n0.setNext(e => console.log(e.getType(), e.getPayload(0)));
-    // mn1.setNext(e => console.log(e.getEmitter().UUID(), e.getType(), e.getPayload(0)));
-    // mn1.subscribe(n0);
+    n0.setNext(e => console.log(e.getType(), e.getPayload(0)));
+    mn1.setNext(e => console.log(e.getEmitter().UUID(), e.getType(), e.getPayload(0)));
+    mn1.subscribe(n0);
     
     mn1.dominate("Test", n1);
     mn1.dominate("Cat", n2);
 
-    // mn1.direct("Cat", (name, node) => {
-    //     return [ node.ID(), name ];
-    // });
-    // mn1.command((name, node) => {
-    //     return [ name, node ];
-    // });
+    mn1.direct("Cat", (name, node) => {
+        return [ node.ID(), name ];
+    });
+    mn1.command((name, node) => {
+        return [ name, node ];
+    });
 
-    // n1.prop("Kitterz", 59);
+    n1.prop("Kitterz", 59);
 
     console.log("*******");
     console.log(mn1.getNodeName(n1));
