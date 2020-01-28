8e4d4f57f51d8bea707247ed4d2c1e2ebdac6d55
mjnrock <mjnrock@umich.edu>
2020-01-28 18:19:15 -0500

    init

--- src/node/data-connector/package.js
+++ src/node/data-connector/package.js
@@ -2,9 +2,9 @@ import Packet from "./Packet";  //! Untested
 
 import WebAPI from "./WebAPI";
 import WebSocket from "./WebSocket";
-import PeerJS from "./PeerJS";
-import ClientNode from "./ClientNode";
-import ServerNode from "./ServerNode";
+import PeerJS from "./$PeerJS";
+import ClientNode from "./$ClientNode";
+import ServerNode from "./$ServerNode";
 
 export default {
     Packet,         //! Untested
