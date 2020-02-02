f0c93c2cfdbd9002f1c3bec43ee8a61eadc939b6
Matt Nawrocki <mjnrock@umich.edu>
2020-02-02 10:37:07 -0500

    init

--- src/react/ReactorComponent.js
+++ src/react/ReactorComponent.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import Context from "./Context";
 
 class ReactorComponent extends Component {
-    static contextType = Context;
-
     constructor(props) {
         super(props);
 
