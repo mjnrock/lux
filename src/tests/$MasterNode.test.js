690b5079be280fd65a3d828ba015d591c67995d3
Matt Nawrocki <mjnrock@umich.edu>
2020-02-02 18:04:00 -0500

    init

--- src/tests/$MasterNode.test.js
+++ src/tests/$MasterNode.test.js
@@ -39,18 +39,27 @@ export function RunTest() {
 
     //* Subscriptions: Reactions and Responses
     let n1 = new Node({ node: 1 });
+    let n2 = new Node({ node: 2 });
     let mn1 = new MasterNode();
     
-    mn1.attach(n1);
-    // n1.listen("prop-change", e => console.log(e.getPayload(0)));     // Listener to visually track events
+
+    mn1.attach("Bob", n1);
+    mn1.attach("Cat", n2);
+    // n1.listen("prop-change", e => console.log(100, e.getPayload(0)));     // Listener to visually track events
+    mn1.addResponse(e => {
+        console.log(99, e.getType());
+    });
 
     n1.prop("node", 5124);
-    n1.prop("node", 99);
+    n1.prop("node", 64);
+    n2.prop("node", "asFWERTWEF");
 
     console.log(n1.getState());
+    console.log(n2.getState());
     console.log(mn1.getState());
+    console.log(mn1.$("Bob"));
+    console.log(mn1.$("Cat"));
     console.log(mn1.$());
-    console.log(mn1.$(n1.UUID()));
 }
 
 RunTest();
