955158a1f53b6ceb01d3c3efb17920abbfd1306b
Matt Nawrocki <mjnrock@umich.edu>
2020-02-12 14:58:57 -0500

    init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import Core from "./core/package";
 import Node from "./node/package";
 import React from "./react/package";
-import Tests from "./tests/package";
 
 export default {
     Core,
     Node,
-    React,
-    Tests
+    React
 };
